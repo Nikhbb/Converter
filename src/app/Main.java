d08bda2629b63c2e552c6fa0c2e1cd3e25b705e2
vnovikov <nik.hbb@gmail.com>
2024-01-21 19:03:52 +0200

    Add project structure

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,31 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        double inputValue = inputValue();
+        boolean isConvertToMiles = isConvertToMiles();
+
+        printResult(inputValue, isConvertToMiles, getResult(inputValue, isConvertToMiles));
+    }
+
+    private static double inputValue(){
+        double value = 0;
+
+        return value;
+    }
+
+    private static  boolean isConvertToMiles(){
+        boolean isConvertToMiles = false;
+
+        return isConvertToMiles;
+    }
+
+    private static double getResult(double inputValue, boolean isConvertToMiles){
+        return -1;
+    }
+
+    private static void printResult(double inputValue, boolean isConvertToMiles, double result){
 
     }
+
+
 }
