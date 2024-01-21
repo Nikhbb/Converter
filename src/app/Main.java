7771c35a54d3522f07e389896ca15aba28089b9d
vnovikov <nik.hbb@gmail.com>
2024-01-21 19:47:21 +0200

    Add print result

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,25 +10,28 @@ public class Main {
         printResult(inputValue, isConvertToMiles, getResult(inputValue, isConvertToMiles));
     }
 
-    private static double inputValue(){
+    private static double inputValue() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введіть значення: ");
         return scanner.nextDouble();
     }
 
-    private static  boolean isConvertToMiles(){
+    private static boolean isConvertToMiles() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Перевести в милі (так=1/ні=0)?  ");
         return (scanner.nextInt() == 1) ? true : false;
     }
 
-    private static double getResult(double inputValue, boolean isConvertToMiles){
+    private static double getResult(double inputValue, boolean isConvertToMiles) {
         return !isConvertToMiles ? inputValue * 1.60934 : 0;
     }
 
-    private static void printResult(double inputValue, boolean isConvertToMiles, double result){
-
+    private static void printResult(double inputValue, boolean isConvertToMiles, double result) {
+        if (!isConvertToMiles) {
+            System.out.println(inputValue + " m = " + result + " km");
+        } else {
+            System.out.println("Convert to kilometers only");
+        }
     }
-
 
 }
